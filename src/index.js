312ee53eb31383721d0678f98184c9756d466f2a
Maria Ozamiz <ozamiz.m@gmail.com>
2020-09-30 11:42:55 +0200

    set project

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './stylesheets/index.css';
+import App from './components/App';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+//Esta página asocia el div id root de la página con el index de public
