00ed588f72b9598a335c7ecfb26b2b627d52b81a
Maria Ozamiz <ozamiz.m@gmail.com>
2020-09-30 12:34:24 +0200

    add TodoList component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
     return (
@@ -8,6 +9,7 @@ function App() {
                 <h1>TO-DO LIST</h1>
             </header>
             <Form />
+            <TodoList />
         </div>
     );
 }
