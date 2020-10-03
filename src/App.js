f79e242bfd025e68de57172158e064ce3aa82302
Maria Ozamiz <ozamiz.m@gmail.com>
2020-10-03 13:15:20 +0200

    filter and render by status

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Form from './components/Form';
 import TodoList from './components/TodoList';
 
@@ -6,6 +6,29 @@ function App() {
     const [inputText, setInputText] = useState('');
     const [todos, setTodos] = useState([]);
     const [status, setStatus] = useState('all');
+    const [filteredTodos, setFilteredTodos] = useState([]);
+
+    useEffect(() => {
+        filterHandler();
+    }, [todos, status]);
+
+    const filterHandler = () => {
+        switch (status) {
+            case 'completed':
+                setFilteredTodos(
+                    todos.filter((item) => item.completed === true)
+                );
+                break;
+            case 'uncompleted':
+                setFilteredTodos(
+                    todos.filter((item) => item.completed === false)
+                );
+                break;
+            default:
+                setFilteredTodos(todos);
+                break;
+        }
+    };
 
     return (
         <div className="App">
@@ -19,7 +42,11 @@ function App() {
                 setTodos={setTodos}
                 setStatus={setStatus}
             />
-            <TodoList todos={todos} setTodos={setTodos} />
+            <TodoList
+                todos={todos}
+                setTodos={setTodos}
+                filteredTodos={filteredTodos}
+            />
         </div>
     );
 }
