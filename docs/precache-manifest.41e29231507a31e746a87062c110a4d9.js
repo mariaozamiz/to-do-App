2f890bb6e7ffa8e74b531381b2c7086df90a6c95
Maria Ozamiz <ozamiz.m@gmail.com>
2020-10-03 14:13:33 +0200

    run docs

---
+++ docs/precache-manifest.41e29231507a31e746a87062c110a4d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faef4f870423edffae8e68a201be972d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c658e89082e8ddf33481",
+    "url": "./static/css/main.fddef4f8.chunk.css"
+  },
+  {
+    "revision": "308c387e28bffc61e0ec",
+    "url": "./static/js/2.72f54182.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.72f54182.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c658e89082e8ddf33481",
+    "url": "./static/js/main.8838d9b2.chunk.js"
+  },
+  {
+    "revision": "e21397ecf58f77891282",
+    "url": "./static/js/runtime-main.8ee73f49.js"
+  }
+]);
